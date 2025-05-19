2e6bab6315b97f2e224210ceda278f4f8cda8da0
Sylvain-TDA <sylvain.teissier@le-campus-numerique.fr>
2025-05-19 15:20:28 +0200

    item.php

---
+++ item.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Produit</title>
+</head>
+<body>
+    
+<?php
+
+$nom = "Nom" ;
+$prix = 0 ;
+$urlImg = "/images"; 
+
+echo $nom;
+echo $prix;
+echo $urlImg;
+?>
+
+</body>
+</html>
